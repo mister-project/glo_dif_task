dbd83d5a7319c667b02635da6617590928c975d1
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-11 07:24:53 +0300

    реализовал только цикл

---
+++ script.js
@@ -0,0 +1,12 @@
+let num = 266219;
+console.log(typeof num);
+let str1 = num.toString(); 
+console.log(str1); 
+let i = 0;
+while (i < 6) { // выводит 0, затем 1, затем 2 э...
+  console.log( str1[i]);
+      i++;
+  console.log( str1[i++]);
+ 
+}
+//console.log( str1[i]) * str1[i++]);
