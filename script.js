b29d650e15f442010967d748d4d6f41f40f3dfc7
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-11 09:52:22 +0300

    вывод из цикла произведения и возведение в степень

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let num = 266219;
-console.log(typeof num);
-let str1 = num.toString(); 
-// console.log(str1); 
+let str1 = num.toString(); //перевод в тип данных string для разбивки числа на цифры
+console.log("Исходное число: " + str1);
+
+
 // let i = 0;
 // while (i < 6) { // выводит 0, затем 1, затем 2 э...
 //   console.log( str1[i]);
@@ -10,11 +11,25 @@ let str1 = num.toString();
 //   console.log( str1[i++]);
  
 // }
-let n = 0;
-let x = str1[n];
-console.log(x);
-while (n < 5) {
-  n++
-x = x * str1[n];
-console.log(x);
+// let n = 0;
+// let x = str1[n];
+// console.log(x);
+// while (n < 5) {
+//   n++
+// x = x * str1[n];
+// console.log(x);
+// }
+ 
+
+
+let y = 1;
+
+for(let i = 0; i < 6; i++) {
+
+ y =  y * str1[i];
+
 }
+
+console.log("п.2) Произведение цифр: " + y); // вывод результата последней итерации цикла 
+const s = y ** 3
+console.log("п.3) возведение в 3 степени : " + s);
