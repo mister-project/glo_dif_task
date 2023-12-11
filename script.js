814a3d1f00a3e593b6ca8527438aa1eae53d72f5
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-11 08:46:02 +0300

    произведение считаем

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let num = 266219;
 console.log(typeof num);
 let str1 = num.toString(); 
-console.log(str1); 
-let i = 0;
-while (i < 6) { // выводит 0, затем 1, затем 2 э...
-  console.log( str1[i]);
-      i++;
-  console.log( str1[i++]);
+// console.log(str1); 
+// let i = 0;
+// while (i < 6) { // выводит 0, затем 1, затем 2 э...
+//   console.log( str1[i]);
+  
+//       i++;
+//   console.log( str1[i++]);
  
+// }
+let n = 0;
+let x = str1[n];
+console.log(x);
+while (n < 5) {
+  n++
+x = x * str1[n];
+console.log(x);
 }
-//console.log( str1[i]) * str1[i++]);
