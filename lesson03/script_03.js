5e9bdd5eafa2317685171c8fc7546c5693021f06
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-12 22:15:42 +0300

    3 урок услож. п. 1 без массива

--- lesson03/script_03.js
+++ lesson03/script_03.js
@@ -1,1 +1,24 @@
-//Начало3
+'use srtict';
+let lang = prompt('на каком языке - ru/en?');
+console.log(lang);
+if (lang === 'ru') {
+    console.log('Понедельник, Вторник, Среда, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else {
+    console.log('введено не верное значение')
+}
+
+// решение через switch - case
+
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Пятница, Суббота, Воскресенье');
+        break
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break
+        default:
+            console.log('введено не верное значение');
+
+}
