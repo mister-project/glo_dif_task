25b27761b03293a675c97ebfa51843f9c1f3b003
Yuriy Kulikov <kulikov-y@ya.ru>
2023-12-13 00:22:08 +0300

    3 урок услож. условно финал

--- lesson03/script_03.js
+++ lesson03/script_03.js
@@ -1,27 +1,40 @@
 'use srtict';
 let lang = prompt('на каком языке - ru/en?');
 console.log(lang);
+
+  
+    
 if (lang === 'ru') {
-    console.log('Понедельник, Вторник, Среда, Пятница, Суббота, Воскресенье');
+    console.log('Понедельник, Вторник, Четверг, Среда, Пятница, Суббота, Воскресенье');
 } else if (lang === 'en') {
     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 } else {
-    console.log('введено не верное значение')
+    console.log('-')
 }
+// // решение через switch - case
 
-// решение через switch - case
-
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Пятница, Суббота, Воскресенье');
+switch (true) {
+    case lang == "ru":
+        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
         break
-    case 'ru':
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    case lang = "en":
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturrday, Sunday');
         break
         default:
-            console.log('введено не верное значение');
+            console.log('-');
 
 }
 
+
+let x = [
+    ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], 
+    ['Даня', 24]
+    ];
+    
+    // отбор строки массива
+    console.log(x[Number(lang =='en')]); 
+    
 const namePerson = prompt('Имя персоны');
+console.log(namePerson);
 namePerson ==='Артем' ? console.log('директор') : namePerson ==='Алексей' ? console.log('преподаватель') : console.log('студент');
